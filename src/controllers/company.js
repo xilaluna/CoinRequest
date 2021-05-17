e36b1ff11737aa2b5ed4ab956b051111fe9bc734
Xila Luna <xilaluna2@gmail.com>
2021-05-17 13:45:02 -0700

    [add] find by name

--- src/controllers/company.js
+++ src/controllers/company.js
@@ -1,5 +1,27 @@
 import express from "express"
+const router = express.Router()
 const Company = require("../models/Company.js")
-/* Routes */
+
+//Retrieves list of all companies
+router.get("/", (req, res) => {
+  Company.find()
+    .then((companies) => {
+      return res.json({ companies })
+    })
+    .catch((err) => {
+      throw err.message
+    })
+})
+
+//Retrieves a company by Name
+router.get("/:companyName", (req, res) => {
+  Company.findOne({ _id: req.params.companyName })
+    .then((companies) => {
+      return res.json({ companies })
+    })
+    .catch((err) => {
+      throw err.message
+    })
+})
 
 module.exports = router
