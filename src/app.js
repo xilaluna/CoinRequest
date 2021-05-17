4f7ee5dfc3f51ca362b4f1d703e6d335db13c79a
Xila Luna <xilaluna2@gmail.com>
2021-05-17 02:52:11 -0700

    [add] app.js server start

--- src/app.js
+++ src/app.js
@@ -9,3 +9,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 require("./controllers/company")(app)
+require("./data/db")
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`API listening on port ${PORT}`)
+})
+
+module.exports = app
